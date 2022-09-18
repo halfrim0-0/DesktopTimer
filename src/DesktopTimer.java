1872bfc224db79aa81f6441cc1ede00e7d426cf4
halfrim0-0 <hitonihiki.develop@gmail.com>
2022-09-18 23:39:05 +0900

    first commit

---
+++ src/DesktopTimer.java
@@ -0,0 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.TimerTask;
+
+public class DesktopTimer extends TimerTask implements ActionListener {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
